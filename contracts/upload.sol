63f9f1c14e7483bd57b932f29dbf983eb14b8b3d
Your Name <sivamrockstar721@gmail.com>
2023-02-08 21:32:01 +0530

    Updated upload, config and deploy

--- contracts/upload.sol
+++ contracts/upload.sol
@@ -1,34 +1,50 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: GPL-3.0
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+pragma solidity >=0.7.0 <0.9.0;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Upload {
+  
+  struct Access{   //structure starts
+     address user;  //address of user
+     bool access; //true or false
+  }
+  mapping(address=>string[]) value; //single mapping storing adresses to strings array
+  mapping(address=>mapping(address=>bool)) ownership; //nested mapping storing boolean value for ownership
+  mapping(address=>Access[]) accessList; //accesslist stores all address that are eleigible to file just like a current entry book
+  mapping(address=>mapping(address=>bool)) previousData; //just like a register it stores all the previous data
 
-    event Withdrawal(uint amount, uint when);
+  function add(address _user,string memory url) external {
+      value[_user].push(url);  //this function takes in IPFS URL and address of image 
+  }
+  function allow(address user) external {//this function allows other user to access the file  
+      ownership[msg.sender][user]=true; //changes the state of other user which has selected to allowed
+      if(previousData[msg.sender][user]){ //this loop iterates and finds if user exists in prevData has true value
+         for(uint i=0;i<accessList[msg.sender].length;i++){ //iteration for loop
+             if(accessList[msg.sender][i].user==user){  //checks if user in slot one of array is equal to current user address
+                  accessList[msg.sender][i].access=true;  //updates access to true
+             }
+         }
+      }else{
+          accessList[msg.sender].push(Access(user,true));  
+          previousData[msg.sender][user]=true;  
+      }
+    
+  }
+  function disallow(address user) public{
+      ownership[msg.sender][user]=false;
+      for(uint i=0;i<accessList[msg.sender].length;i++){ //this loop iterates over the access list and changes the access to false
+          if(accessList[msg.sender][i].user==user){ //checks
+              accessList[msg.sender][i].access=false;  
+          }
+      }
+  }
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+  function display(address _user) external view returns(string[] memory){ //this function takes in user value and shows all the images
+      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access"); 
+      return value[_user];
+  }
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+  function shareAccess() public view returns(Access[] memory){ //this function returns list of all people with the access
+      return accessList[msg.sender];
+  }
 }
